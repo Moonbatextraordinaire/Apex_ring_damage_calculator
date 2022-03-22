012ea24186ed2c80163f240175a20bebc79b8921
Tanguy Clavareau <tanguy.clavareau@student.odisee.be>
2022-03-22 18:45:31 +0100

    now in object

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,163 +1,124 @@
+import logic.Heals;
+import logic.Player;
+import logic.Ring;
+
 import java.util.Scanner;
 
 /**
- * Apex ring damage calculator in funcction of syringes, medkits and gold shield.
+ * Apex ring damage calculator in function of syringes, medkits and gold shield.
  *
  * @author Tanguy Clavareau
  * @version 19/03/2022
  */
+
 public class demo {
     public static void main(String[] args) {
-        int damage = 0;
-        int ring = 0;
-        double timer = 0;
-        double totalTime = 0;
-        int choice = 0;
-
+        Player player1 = new Player();
         Scanner sc = new Scanner(System.in);
-        damage = setDamage(damage, ring, sc);
-        choice = getChoice(choice, sc);
 
-        if (choice == 1) {
-            calculateForSyringes(damage, timer, totalTime);
-        } else if (choice == 2){
-            calculateForSynringesGoldShield(damage, timer, totalTime);
-        }else {
-            calculateForMedkits(damage, timer, totalTime);
+        int ringNumber = getRingNumber(sc);
+        Ring ring = new Ring(ringNumber);
+
+        goldShieldChoice(sc, player1);
+        Heals heal = healSelection(sc);
+
+        float timer = 0;
+        float totalTimer = 0;
+        if (heal == Heals.SYRINGE) {
+            for (; player1.getLife() > 0; player1.setLife(ring.getDamage())) {
+                timer += 1.5;
+                totalTimer += 1.5;
+                if (timer == 7.5) {
+                    timer = 0;
+                    player1.healPlayer(heal);
+                }
+                if (player1.getLife() > 100) {
+                    System.out.println("You'll survive! Go on and stay in that ring buddy");
+                    break;
+                }
+                if (player1.getLife() - ring.getDamage() <= 0) {
+                    System.out.println("Oh no! You would have died in this ring. If you would have constantly healed up, " +
+                                       "It would have taken " + totalTimer + " seconds for you to die");
+                    break;
+                }
+            }
+        } else {
+            for (; player1.getLife() > 0; player1.setLife(ring.getDamage())) {
+                timer += 1.5;
+                totalTimer += 1.5;
+                if (timer == 12) {
+                    timer = 0;
+                    player1.healPlayer(heal);
+                }
+                if (player1.getLife() > 100) {
+                    System.out.println("You'll survive! Go on and stay in that ring buddy");
+                    break;
+                }
+                if (player1.getLife() - ring.getDamage() <= 0) {
+                    System.out.println("Oh no! You would have died in this ring. If you would have constantly healed up, " +
+                                       "It would have taken " + totalTimer + " seconds for you to die");
+                    break;
+                }
+            }
         }
     }
 
-    /**
-     * Asks user to provide ring number. Sets damage based on user input. Checks user input is valid, if invalid loops
-     * until a valid value is given.
-     * @param damage The damage the player will receive, based on the ring parameter.
-     * @param ring The ring number (value between 1-6), set by the user input sc.
-     * @param sc Scanner for user input.
-     * @return Int which will be the damage the player incurs
-     */
-    private static int setDamage(int damage, int ring, Scanner sc) {
+    private static Heals healSelection(Scanner sc) {
+        Heals heal = null;
+        int healChoice = 0;
+        do {
+            System.out.println("How are you going to heal?\n\n1) Syringes\n2) Medkits");
+            try {
+                healChoice = sc.nextInt();
+                if (healChoice == 1) {
+                    heal = Heals.SYRINGE;
+                } else if (healChoice == 2) {
+                    heal = Heals.MEDKIT;
+                } else {
+                    System.out.println("Value given is not 1 or 2");
+                }
+            } catch (Exception e) {
+                System.out.println("That's not a number!");
+                sc.nextLine();
+            }
+        } while (healChoice < 1 || healChoice > 2);
+        return heal;
+    }
+
+    private static void goldShieldChoice(Scanner sc, Player player1) {
+        int goldShieldChoice = 0;
+        do {
+            System.out.println("Does the player have a gold shield?\n\n1) Yes\n2) No");
+            try {
+                goldShieldChoice = sc.nextInt();
+                if (goldShieldChoice == 1) {
+                    player1.setGoldShield(goldShieldChoice);
+                } else if (goldShieldChoice == 2) {
+                    player1.setGoldShield(goldShieldChoice);
+                } else {
+                    System.out.println("Value given is not 1 or 2");
+                }
+            } catch (Exception e) {
+                System.out.println("That's not a number!");
+                sc.nextLine();
+            }
+        } while (goldShieldChoice < 1 || goldShieldChoice > 2);
+    }
+
+    private static int getRingNumber(Scanner sc) {
+        int ringNumber = 0;
         do {
             System.out.println("Please enter a ring number between 1-6");
             try {
-                ring = sc.nextInt();
+                ringNumber = sc.nextInt();
+                if (ringNumber < 1 || ringNumber > 6) {
+                    System.out.println("Provided value is not between 1 and 6");
+                }
             } catch (Exception e) {
-                System.out.println("Invalid value given for ring!");
+                System.out.println("That's not a number!");
                 sc.nextLine();
             }
-        } while(ring < 1 || ring > 6);
-
-        switch (ring) {
-            case (1) -> damage = 2;
-            case (2) -> damage = 3;
-            case (3) -> damage = 10;
-            case (4), (5) -> damage = 20;
-            case (6) -> damage = 25;
-            default -> System.out.println("somehow something went wrong");
-        }
-        return damage;
-    }
-
-    /**
-     * User selection to determine if player heals with syringes, syringes with gold shield or medkits.
-     * Checks user input is valid, if invalid loops until a valid value is given.
-     * @param choice Integer value between 1-3 that is used to determine which heals will be used. Set by user input "sc"
-     * @param sc Scanner for user input
-     * @return Int value between 1-3 that is used to determine which heals will be used.
-     */
-    private static int getChoice(int choice, Scanner sc) {
-        do {
-            System.out.println("Assuming infinite heals, which healing method do you want to calculate ring survivability for? \n" +
-                               "1) Syringes\n" +
-                               "2) Syringe with gold shield\n" +
-                               "3) Medkits");
-            try {
-                choice = sc.nextInt();
-            } catch (Exception e) {
-                System.out.println("Unexpected value...What have you typed in you silly moose!\n");
-                sc.nextLine();
-            } if (choice < 1 || choice > 3) {
-                System.out.println("Selected value is not 1, 2 or 3\n");
-            }
-        } while (choice < 1 || choice > 3);
-        return choice;
-    }
-
-    /**
-     * Calculates if player survives damage incurred based on ring number (set by setDamage method) with a for loop.
-     * @param damage The damage the player will incur each tick.
-     * @param timer Amount of seconds needed for the player to heal. Is calculated as follows: 1.5 sec * x.
-     *             Where "x" is determined by the healing medium (For syringe x =5, for medkit x = 8)
-     * @param totalTime If the player dies, counts the total amount of time it would take for them to die
-     */
-    private static void calculateForSyringes(int damage, double timer, double totalTime) {
-        for (int life = 100; life > 0; life -= damage) {
-            timer += 1.5;
-            totalTime += 1.5;
-            if (timer == 7.5) {
-                timer = 0;
-                life += 25;
-            }
-            if (life > 100) {
-                System.out.println("You'll survive! Go on and stay in that ring buddy");
-                break;
-            }
-            if ((life - damage) <= 0) {
-                System.out.println("Oh no! You would have died in this ring. If you would have constantly healed up, " +
-                                   "It would have taken " + totalTime + " seconds for you to die");
-                break;
-            }
-        }
-    }
-    /**
-     * Calculates if player survives damage incurred based on ring number (set by setDamage method) with a for loop.
-     * @param damage The damage the player will incur each tick.
-     * @param timer Amount of seconds needed for the player to heal. Is calculated as follows: 1.5 sec * x.
-     *             Where "x" is determined by the healing medium (For syringe x =5, for medkit x = 8)
-     * @param totalTime If the player dies, counts the total amount of time it would take for them to die
-     */
-    private static void calculateForSynringesGoldShield(int damage, double timer, double totalTime) {
-        for (int life = 100; life > 0; life -= damage) {
-            timer += 1.5;
-            totalTime += 1.5;
-            if (timer == 7.5) {
-                timer = 0;
-                life += 50;
-            }
-            if (life > 100) {
-                System.out.println("You'll survive! Go on and stay in that ring buddy");
-                break;
-            }
-            if ((life - damage) <= 0) {
-                System.out.println("Oh no! You would have died in this ring. If you would have constantly healed up, " +
-                                   "It would have taken " + totalTime + " seconds for you to die");
-                break;
-            }
-        }
-    }
-    /**
-     * Calculates if player survives damage incurred based on ring number (set by setDamage method) with a for loop.
-     * @param damage The damage the player will incur each tick.
-     * @param timer Amount of seconds needed for the player to heal. Is calculated as follows: 1.5 sec * x.
-     *             Where "x" is determined by the healing medium (For syringe x =5, for medkit x = 8)
-     * @param totalTime If the player dies, counts the total amount of time it would take for them to die
-     */
-    private static void calculateForMedkits(int damage, double timer, double totalTime) {
-        for (int life = 100; life > 0; life -= damage) {
-            timer += 1.5;
-            totalTime += 1.5;
-            if (timer == (12)) {
-                timer = 0;
-                life = 100;
-            }
-            if (life > 100) {
-                System.out.println("You'll survive! Go on and stay in that ring buddy");
-                break;
-            }
-            if ((life - damage) <= 0) {
-                System.out.println("Oh no! You would have died in this ring. If you would have constantly healed up, " +
-                                   "It would have taken " + totalTime + " seconds for you to die");
-                break;
-            }
-        }
+        } while(ringNumber < 1 || ringNumber > 6);
+        return ringNumber;
     }
 }
